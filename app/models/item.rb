d81b2c57e4088b34dfaa74926f679c1a6b7f5dd2
stalimate1027 <81148552+stalimate1027@users.noreply.github.com>
2021-04-27 18:54:18 +0900

    商品価格の上限・下限の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,14 +5,15 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :status, :shipping_source_id, :prefecture_id, :delivery_day_id
 
-  validates :product,            presence: true
-  validates :text,               presence: true
-  validates :category_id,        numericality: { other_than: 0 }
-  validates :status_id,          numericality: { other_than: 0 }
-  validates :price,              presence:true
-  validates :prefecture_id,      numericality: { other_than: 0 }
-  validates :shipping_source_id, numericality: { other_than: 0 }
-  validates :delivery_day_id,    numericality: { other_than: 0 }
-  validates :image,              presence: true
+  validates              :product,            presence: true
+  validates              :text,               presence: true
+  validates              :category_id,        numericality: { other_than: 0 }
+  validates              :status_id,          numericality: { other_than: 0 }
+  validates_inclusion_of :price,              in: 300..9999999
+  validates              :price,              numericality: { with: /\A[0-9]+\z/ }
+  validates              :prefecture_id,      numericality: { other_than: 0 }
+  validates              :shipping_source_id, numericality: { other_than: 0 }
+  validates              :delivery_day_id,    numericality: { other_than: 0 }
+  validates              :image,              presence: true
 
 end
