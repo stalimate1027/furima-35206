63a8700bfdb62e8b0c074debdf3f77c14fb648fe
stalimate1027 <81148552+stalimate1027@users.noreply.github.com>
2021-04-27 16:38:41 +0900

    ActiveHashの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,23 @@ class ItemsController < ApplicationController
 
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:prodcut, :text, :category_id, :status_id, :price, :prefecture_id, :shipping_source_id, :delivery_day_id, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,18 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :status, :shipping_source_id, :prefecture_id, :delivery_day_id
 
   validates :prodcut,            presence: true
   validates :text,               presence: true
-  validates :category_id,        presence: true
-  validates :status_id,          presence: true
+  validates :category_id,        numericality: { other_than: 0 }
+  validates :status_id,          numericality: { other_than: 0 }
   validates :price,              presence:true
-  validates :prefecture_id,      presence: true
-  validates :shipping_source_id, presence: true
-  validates :delivery_day_id,    presence: true
+  validates :prefecture_id,      numericality: { other_than: 0 }
+  validates :shipping_source_id, numericality: { other_than: 0 }
+  validates :delivery_day_id,    numericality: { other_than: 0 }
   validates :image,              presence: true
 
 end
